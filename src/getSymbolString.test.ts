3d79bf052a1104d09ef944882ab376bb65a7267e
usr <usr@example.com>
2023-07-05 18:06:44 +0200

    test: getSymbolString

--- src/getSymbolString.test.ts
+++ src/getSymbolString.test.ts
@@ -1,30 +1,34 @@
 import * as chai from "chai"
 import { createSourceFile, ScriptTarget, createProgram, isFunctionDeclaration, Node } from "typescript"
 import { getSymbolString } from "./getSymbolString"
+import ts from 'typescript'
 
 const expect = chai.expect
 
+const sourceCode = `import fs from 'fs'
+
+/**
+ * This is a test function
+ */
+function testFunction() {
+    // do nothing
+}
+`
 describe("getSymbolString", function () {
-
-  const sourceCode = `
-        /**
-         * This is a test function
-         */
-        function testFunction() {
-            // do nothing
-        }
-    `
-
   const filePath = "test.ts"
   const sourceFile = createSourceFile(filePath, sourceCode, ScriptTarget.ES2015, true)
   const program = createProgram([filePath], {})
-  let funcNode: Node
+  let funcNode: any = null
 
-  sourceFile.forEachChild((node) => {
-    if (isFunctionDeclaration(node)) {
+  sourceFile.forEachChild((node: ts.Node) => {
+    if (ts.isFunctionDeclaration(node)) {
       funcNode = node
     }
   })
+
+  if (!funcNode) {
+    console.log(`error: no funcNode`, sourceFile)
+  }
 
   // Define the set of options to test.
   const options = [
@@ -40,7 +44,7 @@ describe("getSymbolString", function () {
 
   options.forEach((option) => {
     it(`should return correct string for options: ${JSON.stringify(option)}`, function () {
-      const result = getSymbolString(filePath, option, sourceFile, funcNode!, program)
+      const result = getSymbolString(filePath, option, sourceFile, funcNode, program)
       if (option.nofile) {
         expect(result).not.to.contain(`# File: ${filePath}`)
       } else {
@@ -51,11 +55,10 @@ describe("getSymbolString", function () {
       } else {
         expect(result).not.to.contain('This is a test function')
       }
-      // Assuming that the `signature` would be function name in this case
       if (option.signature) {
-        expect(result).to.contain('testFunction')
+        expect(result).to.not.contain('// do nothing')
       } else {
-        expect(result).not.to.contain('testFunction')
+        expect(result).to.contain('// do nothing')
       }
     })
   })
